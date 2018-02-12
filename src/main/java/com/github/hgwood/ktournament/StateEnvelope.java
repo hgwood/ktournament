92067a949fe54a786a47da65fdccdf1f17c478c1
Hugo Wood <hx4.5@free.fr>
2018-02-13 00:28:16 +0100

    typo

--- src/main/java/com/github/hgwood/ktournament/StateEnvelope.java
+++ src/main/java/com/github/hgwood/ktournament/StateEnvelope.java
@@ -7,14 +7,14 @@ import java.util.UUID;
 @Value
 public class StateEnvelope<T extends State> {
     long version;
-    UUID emmittingEventId;
+    UUID emittingEventId;
     T payload;
 
     public static <T extends State> StateEnvelope<T> zero() {
-        return new StateEnvelope<>(0, null, (T) null);
+        return new StateEnvelope<>(0, null, null);
     }
 
-    public StateEnvelope<T> next(UUID emmittingEventId, T payload) {
-        return new StateEnvelope<>(this.version + 1, emmittingEventId, payload);
+    public StateEnvelope<T> next(UUID emittingEventId, T payload) {
+        return new StateEnvelope<>(this.version + 1, emittingEventId, payload);
     }
 }
