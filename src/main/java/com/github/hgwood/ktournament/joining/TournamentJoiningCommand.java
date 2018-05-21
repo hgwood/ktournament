8c8dcd81883f9304a768137ca030e8f04c80dfb5
Hugo Wood <hx4.5@free.fr>
2018-05-21 15:01:11 +0200

    add start tournament to deserializable commands

--- src/main/java/com/github/hgwood/ktournament/joining/TournamentJoiningCommand.java
+++ src/main/java/com/github/hgwood/ktournament/joining/TournamentJoiningCommand.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.github.hgwood.ktournament.framework.Command;
 import com.github.hgwood.ktournament.joining.commands.JoinTournament;
 import com.github.hgwood.ktournament.joining.commands.OpenTournamentToPlayers;
+import com.github.hgwood.ktournament.joining.commands.StartTournament;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
     @JsonSubTypes.Type(name = "OPEN_TOURNAMENT_TO_PLAYERS", value = OpenTournamentToPlayers.class),
-    @JsonSubTypes.Type(name = "JOIN_TOURNAMENT", value = JoinTournament.class)
+    @JsonSubTypes.Type(name = "JOIN_TOURNAMENT", value = JoinTournament.class),
+    @JsonSubTypes.Type(name = "START_TOURNAMENT", value = StartTournament.class)
 })
 public interface TournamentJoiningCommand extends Command<TournamentJoiningState> {
 
